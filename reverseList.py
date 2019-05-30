2a09b4aaf0b8b2d06e866a2178847caba89a32bf
yoyboy <534411590@qq.com>
2019-05-30 21:54:06 +0800

    Create reverseList.py

---
+++ reverseList.py
@@ -0,0 +1,32 @@
+# 翻转一个链表
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = None
+        cur = head
+        while cur:
+            next_ = cur.next
+            cur.next = pre
+            pre = cur
+            cur = next_
+        return pre
+
+def printLink(head):
+    while head:
+        print(head.val)
+        head = head.next
+
+if __name__ == "__main__":
+    head = ListNode(0)
+    cur = head
+    for x in range(1, 10):
+        node = ListNode(x)
+        cur.next = node
+        cur = node
+
+    x = Solution().reverseList(head)
+    printLink(x)
