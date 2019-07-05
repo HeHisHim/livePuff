2bdd29975b9fce1b54c005bfab5bdd21c5b40960
yoyboy <534411590@qq.com>
2019-07-05 15:13:56 +0800

    Example - async execute

--- after_reading/python_multiprocess.py
+++ after_reading/python_multiprocess.py
@@ -1,5 +1,6 @@
 import numpy as np
 from time import time
+import multiprocessing as mp
 
 np.random.RandomState(100)
 arr = np.random.randint(0, 10, size = [200000, 5])
@@ -12,8 +13,10 @@ def solution(arr, minimum = 4, maximun = 8):
             count += 1
     return count
 
-# ---------------不使用并行处理的参考代码---------------
+pool = mp.Pool(mp.cpu_count())
 results = []
+
+# ---------------不使用并行处理的参考代码---------------
 for arr in datas:
     results.append(solution(arr, 4, 8))
 print(results[:5])
@@ -21,9 +24,6 @@ print(results[:5])
 
 
 # ---------------Pool.apply()进行并行化---------------
-import multiprocessing as mp
-pool = mp.Pool(mp.cpu_count())
-results = []
 results = [pool.apply(solution, args = (arr, 4, 8)) for arr in datas]
 pool.close()
 print(results[:5])
@@ -31,9 +31,6 @@ print(results[:5])
 
 
 # ---------------Pool.map()进行并行化---------------
-import multiprocessing as mp
-pool = mp.Pool(mp.cpu_count())
-results = []
 results = pool.map(solution, [arr for arr in datas])
 pool.close()
 print(results[:5])
@@ -44,10 +41,34 @@ print(results[:5])
 # 与Pool.map()一样, Pool.starmap()也仅接受一个迭代器参数
 # 但在starmap()中, 迭代器中的每一个元件也是一个迭代器
 # 实际上, Pool.starmap()就像是一个接受参数的Pool.map()版本
-import multiprocessing as mp
-pool = mp.Pool(mp.cpu_count())
-results = []
 results = pool.starmap(solution, [(arr, 4, 8) for arr in datas])
 pool.close()
 print(results[:5])
 # ---------------Pool.starmap()进行并行化---------------
+
+
+# ---------------Pool.apply_async()进行并行化---------------
+# 此时results是pool.ApplyResult对象的列表, 用get()方法来获取所需的最终结果
+results = [pool.apply_async(solution, args = (arr, 4, 8)) for arr in datas]
+pool.close()
+pool.join()
+# print(results[:5]) # pool.ApplyResult对象
+for res in results[:5]:
+    print(res.get())
+# ---------------Pool.apply_async()进行并行化---------------
+
+
+# ---------------Pool.map_async()进行并行化---------------
+results = pool.map_async(solution, [arr for arr in datas]).get()
+pool.close()
+pool.join()
+print(results[:5])
+# ---------------Pool.map_async()进行并行化---------------
+
+
+# ---------------Pool.starmap_async()进行并行化---------------
+results = pool.starmap_async(solution, [(arr, 4, 8) for arr in datas]).get()
+pool.close()
+pool.join()
+print(results[:5])
+# ---------------Pool.starmap_async()进行并行化---------------
