5a87c7a4a260777334c34f4adb380297b0db8ac0
yoyboy <534411590@qq.com>
2019-08-07 17:54:40 +0800

    main.py flask run

--- flask_project_structure/flask_project_structure/main.py
+++ flask_project_structure/flask_project_structure/main.py
@@ -1,1 +1,13 @@
 # 主执行文件
+from flask import Flask
+
+from flask_project_structure.factory import create_app, setup_logging
+from flask_project_structure import logger
+from flask_project_structure.config import get_config
+
+app = application = create_app(get_config())
+setup_logging(get_config())
+
+@app.route("/")
+def hello():
+    return "Hello, Flask!"
