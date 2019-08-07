e919aeed3a55cb57af96a2752ce44a0bb09734d7
yoyboy <534411590@qq.com>
2019-08-07 17:50:07 +0800

    factory.py - flask config

--- flask_project_structure/flask_project_structure/__init__.py
+++ flask_project_structure/flask_project_structure/__init__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import logging
 
 __version__ = "0.0.1"
--- flask_project_structure/flask_project_structure/factory.py
+++ flask_project_structure/flask_project_structure/factory.py
@@ -1,1 +1,38 @@
 # flask初始化配置
+import logging
+
+from flask import Flask
+from flask_project_structure import logger
+from flask_project_structure.utils.flask_ext import (
+    Mongo, Redis, Mysql
+)
+
+mongo = Mongo(appname="flask_project_structure")
+redis = Redis()
+mysql = Mysql(appname="flask_project_structure")
+
+def _register_blueprint(app: Flask) -> None:
+    # from flask_project_structure.views.xxxx import bp
+    # app.register_blueprint(bp)
+    pass
+
+def _register_extension(app: Flask) -> None:
+    mongo.init_app(app, connect=False)
+    mysql.init_app(app)
+    redis.init_app(app)
+
+def create_app(config: dict) -> Flask:
+    app = Flask("rqams", static_folder=None)
+    app.config.from_mapping(config)
+    level = app.config.get("LOG_LEVEL", "INFO").upper()
+    logger.setLevel(level=getattr(logging, level))
+    logger.debug("system init")
+
+    _register_blueprint(app)
+    _register_extension(app)
+
+    health_check_url = config.get("HEALTH_CHECK_URL")
+    if health_check_url:
+        _register_health_check(app, health_check_url)
+
+    return app
