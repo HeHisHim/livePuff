ce54e67d266ce981add79c1f941f1aa36c0ca9d5
HeHisHim <534411590@qq.com>
2020-05-17 22:15:31 +0800

    Update cache_aside.py

--- usege_redis/cache_aside.py
+++ usege_redis/cache_aside.py
@@ -11,20 +11,53 @@ def mongoProxy():
     database = client.get_database("datahub")
     return database.get_collection("instruments")
 
-def read(order_book_id="000001.XSHE"):
-    redis_client = redisProxy()
-    mongo_client = mongoProxy()
-    redis_cache = redis_client.get(order_book_id)
-    # 如果缓存为空再去mongo拿数据, 否则直接返回
-    if redis_cache is None:
-        data = mongo_client.find_one({"order_book_id": order_book_id}, {"_id": 0})
-        if not data:
-            return
-        redis_client.set(order_book_id, json.dumps(data))
-        print("Read from mongo data")
-        return data
-    print("Read from redis cache")
-    return json.loads(redis_cache)
+# def read(order_book_id="000001.XSHE"):
+#     redis_client = redisProxy()
+#     mongo_client = mongoProxy()
+#     redis_cache = redis_client.get(order_book_id)
+#     # 如果缓存为空再去mongo拿数据, 否则直接返回
+#     if redis_cache is None:
+#         data = mongo_client.find_one({"order_book_id": order_book_id}, {"_id": 0})
+#         if not data:
+#             return
+#         redis_client.set(order_book_id, json.dumps(data))
+#         print("Read from mongo data")
+#         return data
+#     print("Read from redis cache")
+#     return json.loads(redis_cache)
+
+def read_proxy(mongo_proxy, redis_proxy, projection, filter_, cache_key, inside_key=None, cache_type="string", **kwagrs):
+    if "string" == cache_type:
+        redis_cache = redis_proxy.get(cache_key)
+        if redis_cache is None:
+            sort = kwagrs.get(sort)
+            data = mongo_proxy.find_one(projection, filter_, sort=sort)
+            if not data:
+                return
+            redis_proxy.set(cache_key, json.dumps(data))
+            print("Read from mongo data")
+            return data
+        print("Read from redis cache")
+        return json.loads(redis_cache)
+    elif "hash" == cache_type:
+        redis_cache = redis_proxy.hvals(cache_key)
+        if redis_cache is None:
+            sort = kwagrs.get(sort)
+            data = list(mongo_proxy.find(projection, filter_, sort=sort))
+            if not data:
+                return
+            target = kwagrs.get("target")
+            for x in data:
+                redis_proxy.hset(cache_key, {data[target]: json.dumps(data)})
+            print("Read from mongo data")
+            return data
+        print("Read from redis cache")
+        if inside_key is not None:
+            return json.loads(redis_proxy.hget(cache_key, inside_key))
+        else:
+            return [json.loads(x) for x in redis_cache]
+
+
 
 def write(order_book_id="000001.XSHE"):
     redis_client = redisProxy()
@@ -46,5 +79,8 @@ def write(order_book_id="000001.XSHE"):
 if "__main__" == __name__:
     # read_data = read()
     # print(read_data, type(read_data))
-    write_data = write()
-    print(write_data, type(write_data))
+    # write_data = write()
+    # print(write_data, type(write_data))
+    redis_proxy = redisProxy()
+    read_data = read_proxy(None, redis_proxy, None, None, "latest_nv_date", None, cache_type="hash")
+    print(read_data, type(read_data))
