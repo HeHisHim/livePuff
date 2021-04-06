0cd42cc780528c53509e6aec3b619980bd8ea0aa
ye.jinfeng <ye.jinfeng@ricequant.com>
2021-04-06 10:14:41 +0800

    Update futu.py

--- interview/futu/futu.py
+++ interview/futu/futu.py
@@ -2,19 +2,34 @@ import threading
 import time
 import multiprocessing
 
+def synchronized(func):
+    func.__lock__ = threading.Lock()
+    def lock_func(*args, **kwargs):
+        print(func.__lock__, id(func.__lock__))
+        with func.__lock__:
+            return func(*args, **kwargs)
+    return lock_func
+
+SingleTonLock = threading.Lock()
+
 class SingleTon:
+    # 线程锁也必须是单例的
+    @synchronized
     def __new__(cls, *args, **kwargs):
-        if not hasattr(cls, "_instance"):
-            print("not hasattr")
-            cls._instance = object().__new__(cls, *args, **kwargs)
-        return cls._instance
+        print(SingleTonLock, id(SingleTonLock))
+        with SingleTonLock:
+            if not hasattr(cls, "_instance"):
+                time.sleep(1)
+                print("not hasattr")
+                cls._instance = object().__new__(cls, *args, **kwargs)
+            return cls._instance
 
 def func():
     s = SingleTon()
     print(id(s))
 
 if "__main__" == __name__:
-    # idx = [SingleTon() for _ in range(10)]
+    # idx = [func() for _ in range(10)]
     # idx = set(idx)
     # assert 1 == len(idx)
     
