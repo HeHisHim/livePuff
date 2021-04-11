b2d8620aa61ed3cc883389651944e19cf23b92e5
ye.jinfeng <ye.jinfeng@ricequant.com>
2021-04-11 21:49:40 +0800

    Create lru_cache.py

---
+++ record/lru_cache.py
@@ -0,0 +1,43 @@
+from collections import OrderedDict
+import functools
+
+def lru_cache(size):
+    def decorating_function(func):
+        cache = OrderedDict()
+        @functools.wraps(func)
+        def wraps(*args, **kwargs):
+            target = object()
+            if kwargs:
+                key = args + (repr(sorted(kwargs.items())), )
+            else:
+                key = args
+            res = cache.get(key, target)
+            if res is not target:
+                val = cache.pop(key)
+                cache[key] = val
+                print("hit cache")
+                return val
+            val = func(*args, **kwargs)
+            if len(cache) == size:
+                print("cache size full")
+                cache.popitem(last=False)
+                cache[key] = val
+            else:
+                print("cache size not full")
+                cache[key] = val
+            return val
+        return wraps
+    return decorating_function
+
+
+@lru_cache(3)
+def test_lru(*args, **kwargs):
+    print("test_lru: {} - {}".format(args, kwargs))
+
+
+if "__main__" == __name__:
+    test_lru(123, bbc=123)
+    test_lru(223, bbc=123)
+    test_lru(323, bbc=123)
+    test_lru(423, bbc=123)
+    test_lru(323, bbc=123)
